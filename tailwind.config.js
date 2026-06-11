299da350905a27064c6df9be1275e265e68a9aca
chamanaresidencia-spec <chamanaresidencia@gmail.com>
2026-06-10 23:35:09 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'grafite': '#1a1a1a',
+        'ciano-tech': '#00f2ff',
+      },
+    },
+  },
+  plugins: [],
+}
